b4b906a15873455198c3fdacc9f36cf6efe7734c
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-05-21 13:33:02 +0000

    21 May 2023, 19:03

--- src/add-user-use-case.ts
+++ src/add-user-use-case.ts
@@ -7,3 +7,15 @@ export interface AddUserRequest{
 export interface EmailService{
    sendWelcomeLetter(emailAddress:string):void
 }
+
+export class AddUserUseCase{
+   constructor(private emailService:EmailService){}
+
+   public execute(addUserRequest:AddUserRequest){
+    // check if userName is avialbable 
+    // check if email address is available
+    // add user
+
+    this.emailService.sendWelcomeLetter(addUserRequest.emailAddress)
+   }
+}
