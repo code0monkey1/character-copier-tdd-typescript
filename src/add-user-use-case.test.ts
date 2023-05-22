4ea7b1fe9b9a6758008c6dae1b500e3943981652
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-05-22 06:21:59 +0000

    22 May 2023, 11:51

--- src/add-user-use-case.test.ts
+++ src/add-user-use-case.test.ts
@@ -1,11 +1,27 @@
+import { AddUserUseCase, EmailService } from "./add-user-use-case"
+
 describe('add-user-use-case',()=>{
 
    describe('execute',()=>{
+      
+      test('AddUserCuseCase does exist',()=>{
 
+        expect(AddUserUseCase).toBeInstanceOf(Function)
+      })
 
       test('does exist',()=>{
-             
-        expect(true).toBeTruthy()
+        const userRequest={
+          emailAddress:"chiranjeev@gmail.com",
+          userName:"Chiranjeev"
+        }
+        // const emails:string[]=[]
+
+        // const emailService:EmailService=jest.fn(
+            
+        //    emailAddress=>emails.push(emailAddress)
+        // )
+
+        // const sut = new AddUserUseCase(emailService)
            
       })
    })
