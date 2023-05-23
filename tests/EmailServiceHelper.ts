68388292693bf844c238bc96a4054552a78a53f1
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-05-23 15:13:53 +0000

    23 May 2023, 20:43

--- tests/EmailServiceHelper.ts
+++ tests/EmailServiceHelper.ts
@@ -3,7 +3,7 @@ import { EmailService } from "../src/reset-password-use-case";
 class EmailServiceHelper{
       
       private emailsSentTo:string[]=[]
-      
+
       private emailService:EmailService=this.createMockEmailService(email=>this.emailsSentTo.push(email))
       
       private createMockEmailService(captureFn:(email:string)=>void):EmailService{
@@ -13,9 +13,9 @@ class EmailServiceHelper{
             sendMessage:jest.fn(
                message=>captureFn(message.mailTo)
             )
-          }
+            }
 
-     }
+      }
 
       public getSentEmails(){
         
