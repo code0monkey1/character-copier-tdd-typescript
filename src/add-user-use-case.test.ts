dec986b75a52c19f48d7dfccd4c471ac32abaf1b
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-05-23 12:40:19 +0000

    23 May 2023, 18:10

--- src/add-user-use-case.test.ts
+++ src/add-user-use-case.test.ts
@@ -2,7 +2,36 @@ import { AddUserUseCase, EmailService } from "./add-user-use-case"
 
 describe('add-user-use-case',()=>{
 
-   describe('execute',()=>{
+  //  describe('execute',()=>{
+      
+  //     test('AddUserCuseCase does exist',()=>{
+
+  //       expect(AddUserUseCase).toBeInstanceOf(Function)
+  //     })
+
+  //     test('should send welcome letter',()=>{
+  //       const userRequest={
+  //         emailAddress:"chiranjeev@gmail.com",
+  //         userName:"Chiranjeev"
+  //       }
+
+  //       const mockEmailService = new MockEmailService();
+
+  //       const sut = new AddUserUseCase(mockEmailService)
+        
+  //       sut.execute(userRequest)
+
+  //       sut.execute({
+  //         emailAddress:"Crazy@gmail.com",
+  //         userName:"Veeru"
+  //       })
+        
+  //       expect(mockEmailService.wasSendWelcomeLetterCalled).toBeTruthy()
+  //       expect(mockEmailService.sentEmails).toContain("Crazy@gmail.com")
+        
+  //     })
+  //  })
+    describe('execute using jest',()=>{
       
       test('AddUserCuseCase does exist',()=>{
 
@@ -15,7 +44,13 @@ describe('add-user-use-case',()=>{
           userName:"Chiranjeev"
         }
 
-        const mockEmailService = new MockEmailService();
+        const sentWelcomeEmailAddresses:string[]=[]
+
+        const mockEmailService={
+            sendWelcomeLetter:jest.fn(
+              address => sentWelcomeEmailAddresses.push(address)
+            )
+        }
 
         const sut = new AddUserUseCase(mockEmailService)
         
@@ -25,22 +60,21 @@ describe('add-user-use-case',()=>{
           emailAddress:"Crazy@gmail.com",
           userName:"Veeru"
         })
-        
-        expect(mockEmailService.wasSendWelcomeLetterCalled).toBeTruthy()
-        expect(mockEmailService.sentEmails).toContain("Crazy@gmail.com")
+
+        expect(sentWelcomeEmailAddresses).toContain("Crazy@gmail.com")
         
       })
    })
 })
 
 
-        class MockEmailService implements EmailService{
-          public sentEmails:string[]=[]
-          public  wasSendWelcomeLetterCalled:boolean=false;
+        // class MockEmailService implements EmailService{
+        //   public sentEmails:string[]=[]
+        //   public  wasSendWelcomeLetterCalled:boolean=false;
 
-          sendWelcomeLetter(emailAddress: string): void {
-            this.wasSendWelcomeLetterCalled=true;
-            this.sentEmails.push(emailAddress)
-          }
+        //   sendWelcomeLetter(emailAddress: string): void {
+        //     this.wasSendWelcomeLetterCalled=true;
+        //     this.sentEmails.push(emailAddress)
+        //   }
           
-        }
+        // }
