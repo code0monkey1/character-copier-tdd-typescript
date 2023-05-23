a7582f7f4b5955583afa26d72e623768e30f6611
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-05-23 13:02:38 +0000

    23 May 2023, 18:32

--- src/reset-password-use-case.ts
+++ src/reset-password-use-case.ts
@@ -7,3 +7,21 @@ export interface EmailMessage{
   mailTo:string,
   message:string,
 }
+
+
+export class ResetPasswordUseCase{
+
+    constructor(private readonly emailService:EmailService){}
+    
+    public execute(emailAddress:string){
+     
+      const emailMessage:EmailMessage={
+       mailTo:emailAddress,
+       message:"Please reset your email password",
+       subject:"Password Reset"
+
+      }
+      
+      this.emailService.sendMessage(emailMessage)
+    }
+}
