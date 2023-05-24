4286d804790b11f93652bd40a9b6b2c91a6df8de
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-05-24 10:00:28 +0000

    24 May 2023, 15:30

---
+++ src/CharacterCopyHelper.ts
@@ -0,0 +1,29 @@
+import { Destination, Source } from "./copier";
+
+class CharacterCopyHelper{
+
+    private mockWriteChar= jest.fn();
+    private mockReadChar= jest.fn();
+
+
+    public getSource():Source{
+      
+        return {
+
+              readChar:this.mockReadChar
+            
+          }
+    }
+
+    public getDestination():Destination{
+            
+         return { 
+           
+              writeChar:this.mockWriteChar
+
+            } 
+
+     }
+
+
+}
